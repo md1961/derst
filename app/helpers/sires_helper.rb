e9179ee48b579ebb214a321214881740886003a9
Naoyuki Kumagai <kumagai@{{ remote_hostname }}>
2019-09-05 10:56:11 +0900

    Implement 'sires#show' to display bloodline

--- app/helpers/sires_helper.rb
+++ app/helpers/sires_helper.rb
@@ -3,4 +3,10 @@ module SiresHelper
   def trait_names
     %i[fee distances growth dirt temper contend health achievement stability]
   end
+
+  def father_in_bloodline(sire, generation, number)
+    father = sire.bloodline_father(generation, number)
+    return nil unless father
+    father.name_eng || father.name_jp
+  end
 end
