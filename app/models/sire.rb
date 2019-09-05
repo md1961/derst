a18753e7f6420c5c3e94db07301e886f595c1dc1
Naoyuki Kumagai <kumagai@{{ remote_hostname }}>
2019-09-05 10:51:02 +0900

    Add Sire#bloodline_father()

--- app/models/sire.rb
+++ app/models/sire.rb
@@ -4,6 +4,7 @@ class Sire < ApplicationRecord
   has_one :trait, class_name: 'SireTrait', dependent: :destroy
   has_many :sire_inbreed_effects, dependent: :destroy
   has_many :inbreed_effects, through: :sire_inbreed_effects
+  has_many :sire_maternal_lines, dependent: :destroy
 
   scope :breedable, -> { joins(:trait).where.not(name_jp: nil) }
 
@@ -21,5 +22,53 @@ class Sire < ApplicationRecord
 
   def name
     name_jp || name_eng
+  end
+
+  def bloodline_father(generation, number = nil)
+    case generation
+    when 1
+      father
+    when 2
+      case number
+      when 1
+        father.father
+      else
+        sire_maternal_lines.find_by(generation: 2)&.father
+      end
+    when 3
+      case number
+      when 1
+        father.father&.father
+      when 2
+        father.bloodline_father(2)
+      when 3
+        bloodline_father(2)&.father
+      else
+        sire_maternal_lines.find_by(generation: 3)&.father
+      end
+    when 4
+      case number
+      when 1
+        bloodline_father(3, 1)&.father
+      when 2
+        bloodline_father(2, 1)&.bloodline_father(2)
+      when 3
+        bloodline_father(3, 2)&.father
+      when 4
+        bloodline_father(2, 2)&.bloodline_father(2)
+      when 5
+        bloodline_father(3, 3)&.father
+      when 6
+        bloodline_father(3, 3)&.bloodline_father(2)
+      when 7
+        bloodline_father(3, 4)&.father
+      else
+        sire_maternal_lines.find_by(generation: 4)&.father
+      end
+    end
+  end
+
+  def to_s
+    name
   end
 end
