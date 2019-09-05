99876c1ec5a40cca393cc771449b39aed271a4ee
Naoyuki Kumagai <kumagai@{{ remote_hostname }}>
2019-09-05 13:20:00 +0900

    Extract partial 'bloodline' from 'sires#show'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def father_in_bloodline(horse, generation, number)
+    father = horse.bloodline_father(generation, number)
+    if father
+      father.name_eng || father.name_jp
+    else
+      render partial: 'father_input', locals: {generation: generation, number: number}
+    end
+  end
 end
--- app/helpers/sires_helper.rb
+++ app/helpers/sires_helper.rb
@@ -3,13 +3,4 @@ module SiresHelper
   def trait_names
     %i[fee distances growth dirt temper contend health achievement stability]
   end
-
-  def father_in_bloodline(sire, generation, number)
-    father = sire.bloodline_father(generation, number)
-    if father
-      father.name_eng || father.name_jp
-    else
-      render partial: 'father_input', locals: {generation: generation, number: number}
-    end
-  end
 end
