8fcddbfe39f461695d6a0206ab90ad024d52cdee
Naoyuki Kumagai <kumagai@{{ remote_hostname }}>
2019-09-06 14:52:08 +0900

    Fix sire_filter_params to return empty hash unless params[:sire_filter]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   private
 
     def sire_filter_params
+      return {} unless params[:sire_filter]
       params.require(:sire_filter).permit(:stability)
     end
 end
