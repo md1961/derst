0f509d3e8007bfcb0161ce0df8a96e6ba3c48e15
Naoyuki Kumagai <kumagai@namaka>
2019-09-10 11:09:06 +0900

    Mare to include BloodlineTrackable and belongs_to :father

--- app/models/mare.rb
+++ app/models/mare.rb
@@ -1,5 +1,9 @@
 class Mare < ApplicationRecord
+  include BloodlineTrackable
+
   self.inheritance_column = :_type_disabled
 
+  belongs_to :father, class_name: 'Sire', optional: true
   belongs_to :lineage
+  has_many :maternal_lines, class_name: 'MareMaternalLine', dependent: :destroy
 end
