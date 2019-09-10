4921e5a2e16ad49768b0a3b409218ad7452c1178
Naoyuki Kumagai <kumagai@namaka>
2019-09-10 11:26:22 +0900

    Implement 'mares#index'

--- app/controllers/mares_controller.rb
+++ app/controllers/mares_controller.rb
@@ -1,6 +1,12 @@
 class MaresController < ApplicationController
 
   def index
+    @mares = Mare.order(:price, :name)
+
+    if v = params[:entering_bloodline]
+      session[:entering_bloodline_for_mare] = v == 'true'
+    end
+    @entering_bloodline = session[:entering_bloodline_for_mare]
   end
 
   def show
--- app/helpers/mares_helper.rb
+++ app/helpers/mares_helper.rb
@@ -1,2 +1,6 @@
 module MaresHelper
+
+  def trait_names
+    %i[lineage price speed stamina has_child rating type]
+  end
 end
