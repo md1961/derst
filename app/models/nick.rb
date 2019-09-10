c3be936c161f149182008978c6d047603cb24630
Naoyuki Kumagai <kumagai@namaka>
2019-09-11 08:30:05 +0900

    Add Nick.good?()

--- app/models/nick.rb
+++ app/models/nick.rb
@@ -1,4 +1,11 @@
 class Nick < ApplicationRecord
   belongs_to :lineage1, class_name: 'Lineage'
   belongs_to :lineage2, class_name: 'Lineage'
+
+  def self.good?(sire, mare)
+    sire_lineage = sire.trait.lineage
+    mare_lineage = mare.lineage
+       exists?(lineage1: sire_lineage, lineage2: mare_lineage) \
+    || exists?(lineage1: mare_lineage, lineage2: sire_lineage)
+  end
 end
