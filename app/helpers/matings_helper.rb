3c3ff51f32e1118f8a000afd4b1823c691f225b7
Naoyuki Kumagai <kumagai@namaka>
2019-09-11 08:48:36 +0900

    Modify helper inbreed_display() to omit rest if thicker than '3x3'

--- app/helpers/matings_helper.rb
+++ app/helpers/matings_helper.rb
@@ -1,6 +1,17 @@
 module MatingsHelper
 
   def inbreed_display(mating)
-    mating.h_inbreeds.map { |f, gs| "#{f.name} #{gs.join('x')}" }.join(', ')
+    h_inbreeds = mating.h_inbreeds
+    additional = ""
+
+    thickest = h_inbreeds.values.sort.first
+    if [[2, 2], [2, 3], [3, 3]].include?(thickest)
+      h_inbreeds = h_inbreeds.reject { |_, v| v != thickest }.to_h
+      additional = ", ..."
+    end
+
+    h_inbreeds.map { |father, generations|
+      "#{father.name} #{generations.join('x')}"
+    }.join(', ') + additional
   end
 end
