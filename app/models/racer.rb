dfdfba48855a8d12a897258257f546f6ecead9bc
Naoyuki Kumagai <kumagai@namaka>
2019-09-11 14:05:38 +0900

    Add Racer#age()

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,4 +1,9 @@
 class Racer < ApplicationRecord
   belongs_to :ranch
   belongs_to :stable, optional: true
+
+  def age
+    return nil unless year_birth
+    ranch.year - year_birth + 1
+  end
 end
