b14b0cc3d54b7d7c81616aa7d6f1d7b780c72327
Naoyuki Kumagai <kumagai@namaka>
2019-09-12 08:55:06 +0900

    Modify 'ranches#show' to emphasize best weight of each racer

--- app/controllers/ranches_controller.rb
+++ app/controllers/ranches_controller.rb
@@ -4,6 +4,6 @@ class RanchesController < ApplicationController
     @ranch = Ranch.last
     @mares  = @ranch.mares
     @racers = @ranch.racers
-    @racer_to_edit = Racer.find_by(id: params[:racer_id_to_edit])
+    @racer_id_to_edit = params[:racer_id_to_edit].to_i
   end
 end
