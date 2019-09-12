c0a7ac2308b21c1329f2ab84f246fbea4fd6475b
Naoyuki Kumagai <kumagai@namaka>
2019-09-12 15:58:00 +0900

    Add Ranch#go_to_next_week()

--- app/models/ranch.rb
+++ app/models/ranch.rb
@@ -2,4 +2,17 @@ class Ranch < ApplicationRecord
   has_many :ranch_mares, dependent: :destroy
   has_many :mares, through: :ranch_mares
   has_many :racers
+
+  def go_to_next_week
+    attrs = {week: week + 1}
+    if attrs[:week] > 4
+      attrs[:week] = 1
+      attrs[:month] = month + 1
+      if attrs[:month] > 12
+        attrs[:month] = 1
+        attrs[:year] = year + 1
+      end
+    end
+    update!(attrs)
+  end
 end
