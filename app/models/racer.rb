dc1258c314f7a4cf187a3afff644f255a11e6c3f
Naoyuki Kumagai <kumagai@namaka>
2019-09-13 09:17:33 +0900

    Add Racer#age=()

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -12,4 +12,8 @@ class Racer < ApplicationRecord
     return nil unless year_birth
     ranch.year - year_birth + 1
   end
+
+  def age=(value)
+    self.year_birth = value.blank? ? nil : ranch.year - value.to_i + 1
+  end
 end
