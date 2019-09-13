551ca81774ac8310276963f1c6b05ea016ecef51
Naoyuki Kumagai <kumagai@namaka>
2019-09-13 10:49:31 +0900

    Correct 'matings#index' to go back to 'ranches#show' even after filtering

--- app/controllers/sire_filters_controller.rb
+++ app/controllers/sire_filters_controller.rb
@@ -2,6 +2,7 @@ class SireFiltersController < ApplicationController
 
   def create
     path_name = params[:mare_id].to_i > 0 ? :matings_path : :sires_path
-    redirect_to send(path_name, sire_filter: sire_filter_params, mare_id: params[:mare_id])
+    redirect_to send(path_name, sire_filter: sire_filter_params,
+                     mare_id: params[:mare_id], ranch_id: params[:ranch_id])
   end
 end
