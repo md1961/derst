8864ee449c3b91dc1f4ddbffb587545cec72d9d6
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2019-09-15 09:50:36 +0900

    Add Grade.open and Grade#high_stake?()

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,5 +1,13 @@
 class Grade < ApplicationRecord
 
+  def self.open
+    @@grade_open ||= find_by(abbr: 'OP')
+  end
+
+  def high_stake?
+    name.starts_with?('G')
+  end
+
   def to_s
     name
   end
