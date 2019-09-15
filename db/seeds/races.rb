4312023f1a38e7d57bcbd251ddb1cc7dba55678e
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2019-09-15 10:28:43 +0900

    Finish seeds for Race of month 1

--- db/seeds/races.rb
+++ db/seeds/races.rb
@@ -1,8 +1,6 @@
-[
-  Race
-].each do |model|
-  model.destroy_all
-end
+month = 1
+
+Race.where(month: 1).destroy_all
 
 [
   %w[1 1 中山 4 m nil nil 0 d 1200 a 0],
@@ -41,6 +39,42 @@ end
   %w[1 2 京都 5u 9 睦月賞 nil 0 t 2400 a 0],
   %w[1 2 京都 5u 16 寿S nil 0 t 1600 a 0],
   %w[1 2 京都 5u g3 平安S nil 0 d 1800 s 0],
+  %w[1 3 中山 4 m nil nil 0 d 1200 a 0],
+  %w[1 3 中山 4 m nil nil 2 d 1800 a 0],
+  %w[1 3 中山 4 5 黒竹賞 nil 0 d 1600 a 0],
+  %w[1 3 中山 4 o フローラS nil 1 t 1200 a 0],
+  %w[1 3 中山 5u 5 nil nil 0 d 1800 a 0],
+  %w[1 3 中山 5u 9 nil nil 0 d 1800 a 0],
+  %w[1 3 中山 5u 9 初春賞 nil 0 t 2500 a 0],
+  %w[1 3 中山 5u 16 初富士S nil 0 t 1600 h 0],
+  %w[1 3 中山 5u g3 ガーネットS nil 0 d 1200 s 0],
+  %w[1 3 京都 4 m nil nil 0 d 1400 a 0],
+  %w[1 3 京都 4 m nil nil 0 d 1800 a 0],
+  %w[1 3 京都 4 5 寒梅賞 nil f d 1400 a 0],
+  %w[1 3 京都 4 g3 シンザン記念 nil 0 t 1600 s 0],
+  %w[1 3 京都 5u 5 nil nil 0 d 1200 a 0],
+  %w[1 3 京都 5u 9 nil nil 0 d 1800 a 0],
+  %w[1 3 京都 5u 9 八坂特別 nil 0 d 1400 a 0],
+  %w[1 3 京都 5u 16 雅S nil 0 t 1800 h 0],
+  %w[1 3 京都 5u o 洛陽S nil 0 t 1600 s 0],
+  %w[1 4 中山 4 m nil nil f d 1200 a 0],
+  %w[1 4 中山 4 m nil nil 0 d 1800 a 0],
+  %w[1 4 中山 4 5 呉竹賞 nil 0 t 1200 a 0],
+  %w[1 4 中山 4 o ジュニアC nil 0 t 2000 s 0],
+  %w[1 4 中山 5u 5 nil nil 0 d 1200 a 0],
+  %w[1 4 中山 5u 9 nil nil 0 d 1200 a 0],
+  %w[1 4 中山 5u 9 若潮賞 nil 0 t 1800 h 0],
+  %w[1 4 中山 5u 16 ジャニュアリーS nil 0 t 2000 h 0],
+  %w[1 4 中山 5u g2 AJCC nil 0 t 2200 s 0],
+  %w[1 4 京都 4 m nil nil 0 d 1200 a 0],
+  %w[1 4 京都 4 m nil nil f d 1800 a 0],
+  %w[1 4 京都 4 5 白梅賞 nil 0 d 1800 a 0],
+  %w[1 4 京都 4 o 若駒S nil 0 t 2000 s 0],
+  %w[1 4 京都 5u 5 nil nil 0 d 1800 a 0],
+  %w[1 4 京都 5u 9 nil nil 0 d 1200 a 0],
+  %w[1 4 京都 5u 9 稲荷特別 nil 0 t 1800 a 0],
+  %w[1 4 京都 5u 16 石清水S nil 0 t 1200 a 0],
+  %w[1 4 京都 5u g2 日経新春杯 nil 0 t 2400 h 0],
   # enum limitation: {unrestricted: 0, female_only: 1, domestic_only: 2}
   # age constant separate handicap
 
@@ -74,7 +108,7 @@ end
     grade:      grade,
     name:       name == 'nil' ? nil : name,
     abbr:       abbr,
-    limitation: limitation.to_i,
+    limitation: limitation == 'f' ? 1 : limitation == 'd' ? 2 : limitation.to_i,
     is_turf:    is_turf,
     distance:   distance.to_i,
     weight:     weight,
