ce31e11963bbf9aec9d9c4a395260756e49134b8
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2019-09-16 12:07:30 +0900

    Add Course#same_from?() and #on_the_day_from?()

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,5 +1,9 @@
 class Area < ApplicationRecord
 
+  def same_from?(center)
+    self == center.area
+  end
+
   def on_the_day_from?(center)
     (location - center.area.location).abs <= 2
   end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,14 @@
 class Course < ApplicationRecord
   belongs_to :area
 
+  def same_from?(stable)
+    area.same_from?(stable.center)
+  end
+
+  def on_the_day_from?(stable)
+    area.on_the_day_from?(stable.center)
+  end
+
   def to_s
     name
   end
