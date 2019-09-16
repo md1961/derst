54ba0161a7a99dd19a7a3f774cbc5cc888b63001
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2019-09-16 13:09:01 +0900

    Modify 'races#index' by adding links to prev and next month

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,7 +1,7 @@
 class RacesController < ApplicationController
 
   def index
-    month = params[:month] || 1
-    @races_by_course = Race.where("month = ?", month).group_by(&:course)
+    @month = (params[:month] || 1).to_i
+    @races_by_course = Race.where("month = ?", @month).group_by(&:course)
   end
 end
