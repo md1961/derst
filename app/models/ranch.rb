18b0722a7edabfef5c630921dcdfacf26d3c2102
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2019-09-16 13:23:04 +0900

    Modify 'racers#show' to display weeks later of race candidates

--- app/models/ranch.rb
+++ app/models/ranch.rb
@@ -3,6 +3,18 @@ class Ranch < ApplicationRecord
   has_many :mares, through: :ranch_mares
   has_many :racers
 
+  def weeks_later_of(month, week)
+    if month == self.month
+      diff = week - self.week
+      diff += 4 * 12 if diff < 0
+    else
+      month_diff = month - self.month
+      month_diff + 12 if month_diff < 0
+      diff = month_diff * 4 + week - self.week
+    end
+    diff
+  end
+
   def go_to_next_week
     attrs = {week: week + 1}
     if attrs[:week] > 4
