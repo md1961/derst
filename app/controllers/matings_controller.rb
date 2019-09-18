1a0b07c75f7b614f57e74975cedf475a3ccb9d94
Naoyuki Kumagai <kumagai@namaka>
2019-09-18 11:48:30 +0900

    Alter 'matings#index' not to display inbreeds initially

--- app/controllers/matings_controller.rb
+++ app/controllers/matings_controller.rb
@@ -2,12 +2,11 @@ class MatingsController < ApplicationController
 
   def index
     @mare = Mare.find(params[:mare_id])
-
     @sire_filter = SireFilter.new(sire_filter_params)
-
     @sires = Sire.includes(:trait)
               .breedable.where(@sire_filter.conditions)
               .order(fee: :asc, name_jp: :desc)
+    @displays_inbreeds = params[:displays_inbreeds] == 'true'
 
     @ranch = Ranch.find_by(id: params[:ranch_id])
   end
