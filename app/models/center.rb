dca0e3a248abe5c7cf113518f7ff8eddb1ca844c
Naoyuki Kumagai <kumagai@namaka>
2019-09-20 08:58:37 +0900

    Add Jockey#center_and_stable()

--- app/models/center.rb
+++ app/models/center.rb
@@ -4,4 +4,12 @@ class Center < ApplicationRecord
     area_name = name == '美浦' ? 'Kanto' : 'Kansai'
     Area.find_by(name: area_name)
   end
+
+  def short_stay?
+    name == '短期'
+  end
+
+  def to_s
+    name
+  end
 end
--- app/models/jockey.rb
+++ app/models/jockey.rb
@@ -1,5 +1,10 @@
 class Jockey < ApplicationRecord
+  belongs_to :center
   belongs_to :stable, optional: true
+
+  def center_and_stable
+    "#{center}#{center.short_stay? ? '' : stable ? ' 専属' : ''}"
+  end
 
   def to_s
     name
