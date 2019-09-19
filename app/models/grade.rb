651c9898abc8dd76800af6d4e72b356822e2f882
Naoyuki Kumagai <kumagai@namaka>
2019-09-19 11:33:44 +0900

    Add Grade#one_down()

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -10,6 +10,10 @@ class Grade < ApplicationRecord
     name.starts_with?('G')
   end
 
+  def one_down
+    self.class.where("ordering < ?", ordering).order(ordering: :desc).first
+  end
+
   def to_s
     name
   end
