5fe4ef197e02a0ea6ec19b2bb9d026a6ffc5577d
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2019-09-20 21:00:33 +0900

    Add scope TargetRace.in_week_of()

--- app/models/target_race.rb
+++ app/models/target_race.rb
@@ -1,4 +1,8 @@
 class TargetRace < ApplicationRecord
   belongs_to :racer
   belongs_to :race
+
+  scope :in_week_of, ->(month_week) {
+    joins(:race).where("month = ? AND week = ?", *month_week.to_a)
+  }
 end
