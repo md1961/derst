a96b654318017c2b80819843f3e60ee9104161c2
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2019-09-20 21:03:02 +0900

    Modify to destroy target races in week just left

--- app/controllers/ranches_controller.rb
+++ app/controllers/ranches_controller.rb
@@ -10,6 +10,7 @@ class RanchesController < ApplicationController
   def next_week
     ranch = Ranch.find(params[:id])
     racer = Racer.find_by(id: params[:racer_id])
+    TargetRace.in_week_of(ranch.month_week).destroy_all
     ranch.go_to_next_week
     redirect_to racer || ranch
   end
