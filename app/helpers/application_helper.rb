9bb1170e09dfb6f03ca66b569465fd9693d2fc19
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2019-09-22 15:52:24 +0900

    Modify 'racers#show' to display own inbreed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -67,6 +67,24 @@ module ApplicationHelper
     end
   end
 
+  def inbreed_display(mating)
+    h_inbreeds = mating.h_inbreeds
+    additional = ""
+
+    thickest = h_inbreeds.values.sort.first
+    if [[2, 2], [2, 3], [3, 3]].include?(thickest)
+      h_inbreeds = h_inbreeds.reject { |_, v| v != thickest }.to_h
+      additional = ", ..."
+    end
+
+    h_inbreeds.map { |father, generations|
+      effects = ""
+      effects = "(#{father.inbreed_effects.map(&:abbr).join(' ')})" \
+        unless father.inbreed_effects.empty?
+      "#{father.name}#{effects} #{generations.join('x')}"
+    }.join(', ') + additional
+  end
+
   def race_age_display(age)
     age.sub(/\A(\d)/, '\1歳').sub('U', '上')
   end
