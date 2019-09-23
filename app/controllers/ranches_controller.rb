a1c22d66b9991da1e9821093506d20ae3e617a45
Naoyuki Kumagai <kumagai@namaka>
2019-09-24 08:47:21 +0900

    Modify 'ranches#show' to display active racers only

--- app/controllers/ranches_controller.rb
+++ app/controllers/ranches_controller.rb
@@ -3,7 +3,7 @@ class RanchesController < ApplicationController
   def show
     @ranch = Ranch.last
     @mares  = @ranch.mares
-    @racers = @ranch.racers
+    @racers = @ranch.racers.active
     @racer_id_to_edit = params[:racer_id_to_edit].to_i
   end
 
