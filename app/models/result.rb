98f5be5e906e91e31198fab54fc6607ff1c5e01b
Naoyuki Kumagai <kumagai@namaka>
2019-09-26 08:30:29 +0900

    Add Result#net_prize()

--- app/models/result.rb
+++ app/models/result.rb
@@ -3,4 +3,9 @@ class Result < ApplicationRecord
   belongs_to :race
   belongs_to :jockey, optional: true
   has_one :post_race
+
+  def net_prize
+    return 0 unless place
+    race.net_prize_for(place)
+  end
 end
