ef0f63899e3066fd1f0c59fb9fd2b1557c9b3d3f
Naoyuki Kumagai <kumagai@namaka>
2019-09-25 09:56:15 +0900

    Refactor MatingListsController by extracting before_action :set_mating

--- app/controllers/mating_lists_controller.rb
+++ app/controllers/mating_lists_controller.rb
@@ -1,4 +1,5 @@
 class MatingListsController < ApplicationController
+  before_action :set_mating, only: %i[update delete]
   before_action :set_mating_list
 
   KEY_MATING_LIST_IN_JSON = :mating_list_in_json
@@ -7,26 +8,26 @@ class MatingListsController < ApplicationController
   end
 
   def update
-    sire = Sire.find_by_name( params[:sire])
-    mare = Mare.find_by(name: params[:mare])
-    mating = Mating.new(mare, sire)
-    @mating_list << mating
+    @mating_list << @mating
 
     save_mating_list
     redirect_to mating_list_path
   end
 
   def delete
-    sire = Sire.find(params[:sire_id])
-    mare = Mare.find(params[:mare_id])
-    mating = Mating.new(mare, sire)
-    @mating_list.delete(mating)
+    @mating_list.delete(@mating)
 
     save_mating_list
     redirect_to mating_list_path
   end
 
   private
+
+    def set_mating
+      sire = Sire.find_by_name( params[:sire]) || Sire.find(params[:sire_id])
+      mare = Mare.find_by(name: params[:mare]) || Mare.find(params[:mare_id])
+      @mating = Mating.new(mare, sire)
+    end
 
     def set_mating_list
       @mating_list = MatingList.build_from_json(session[KEY_MATING_LIST_IN_JSON])
