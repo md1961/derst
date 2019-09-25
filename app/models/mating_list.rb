6ecfef94ef2a4df911d3d1259c0ecc51b4bd6a5a
Naoyuki Kumagai <kumagai@namaka>
2019-09-25 09:18:20 +0900

    Add value object MatingList

---
+++ app/models/mating_list.rb
@@ -0,0 +1,28 @@
+class MatingList
+
+  def self.build_from_json(json)
+    json = "[]" if json.blank?
+    new(JSON.parse(json).map { |mare_id, sire_id|
+      Mating.new(Mare.find(mare_id), Sire.find(sire_id))
+    })
+  end
+
+  def initialize(matings)
+    @matings = matings
+  end
+
+  def each(&block)
+    @matings.each(&block)
+  end
+
+  def <<(mating)
+    @matings << mating
+    self
+  end
+
+  def to_json_in_session
+    JSON.generate(@matings.map { |mating|
+      [mating.mare.id, mating.sire.id]
+    })
+  end
+end
