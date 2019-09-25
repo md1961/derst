afa046afc4630c466f9e7de5c24e5709c015df66
Naoyuki Kumagai <kumagai@namaka>
2019-09-25 11:21:05 +0900

    Implement 'ranch_mares#destroy'

--- app/controllers/ranch_mares_controller.rb
+++ app/controllers/ranch_mares_controller.rb
@@ -3,4 +3,11 @@ class RanchMaresController < ApplicationController
   def new
     @ranch_mare = RanchMare.new(ranch_id: params[:ranch_id])
   end
+
+  def destroy
+    ranch_mare = RanchMare.find(params[:id])
+    ranch = ranch_mare.ranch
+    ranch_mare.destroy
+    redirect_to ranch
+  end
 end
