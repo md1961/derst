2197f64b0cfcf608517e296579ac50421c611ec2
Naoyuki Kumagai <kumagai@namaka>
2019-09-26 15:06:28 +0900

    Refactor racer list to use scope Racer.older_first

--- app/controllers/ranches_controller.rb
+++ app/controllers/ranches_controller.rb
@@ -3,7 +3,7 @@ class RanchesController < ApplicationController
   def show
     @ranch = Ranch.last
     @mares  = @ranch.mares
-    @racers = @ranch.racers.active
+    @racers = @ranch.racers.active.older_first
     @racer_id_to_edit = params[:racer_id_to_edit].to_i
   end
 
