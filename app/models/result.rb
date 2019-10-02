5d589487db6697f471cae7ce17041cca215556c1
Naoyuki Kumagai <kumagai@namaka>
2019-10-02 10:18:16 +0900

    Add Result#set_load_from_racer_and_race!()

--- app/models/result.rb
+++ app/models/result.rb
@@ -8,4 +8,41 @@ class Result < ApplicationRecord
     return 0 unless place
     race.net_prize_for(place)
   end
+
+  def set_load_from_racer_and_race!
+    return unless racer
+    return unless race&.age_constant?
+    age = racer.age
+    month = racer.ranch.month
+    value = if race.grade.abbr == '新'
+              53
+            elsif race.grade.abbr == '未'
+              if racer.female?
+                53
+              elsif age == 3
+                54
+              else
+                55
+              end
+            elsif racer.female?
+              if age <= 4
+                53
+              elsif age == 5 and month <= 6
+                54
+              else
+                55
+              end
+            else
+              if age == 3
+                54
+              elsif age == 4
+                55
+              elsif (age == 5 and month <= 6) || age >= 7
+                56
+              else
+                57
+              end
+            end
+    update!(load: value)
+  end
 end
