4b96b3a7683bfb136d0df2699e412ee78b4f3c19
Naoyuki Kumagai <kumagai@namaka>
2019-10-08 11:55:30 +0900

    Modify retired racer list to display father, mother, records and net prize

--- app/models/mare.rb
+++ app/models/mare.rb
@@ -6,4 +6,8 @@ class Mare < ApplicationRecord
   belongs_to :father, class_name: 'Sire', optional: true
   belongs_to :lineage
   has_many :maternal_lines, class_name: 'MareMaternalLine', dependent: :destroy
+
+  def to_s
+    name
+  end
 end
