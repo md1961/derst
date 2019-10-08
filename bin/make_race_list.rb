f37833187e8c54d69685180794c493aee48e4357
Naoyuki Kumagai <kumagai@namaka>
2019-10-08 14:45:52 +0900

    Alter bin/make_race_list.rb to prepare loads for 'separate'

--- bin/make_race_list.rb
+++ bin/make_race_list.rb
@@ -17,14 +17,15 @@ using StringConsideringMultiByteLength
 
 max_name_len = Race.pluck(:name).compact.map(&:mb_length).max
 
-Race.includes(:grade).order(:age, :grade_id, :month, :week).find_all { |race|
-  race.grade.high_stake?
+Race.separate
+    .includes(:grade).order(:grade_id, :age, :month, :week).find_all { |race|
+  true
 }.each do |race|
   puts [
     race.id.to_s.ljust(7),
     race.age.ljust(2),
     race.course,
-    race.grade,
+    race.grade.abbr,
     race.month_week.to_s.rjust(4),
     race.name.mb_ljust(max_name_len),
     '= '
