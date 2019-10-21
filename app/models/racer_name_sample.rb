db59d8ef4eee02d9f8f2673e45f3254ef87e8266
Naoyuki Kumagai <kumagai@namaka>
2019-10-21 13:40:59 +0900

    Modify RacerNameSample to convert name to katakana on .new()

--- app/models/racer_name_sample.rb
+++ app/models/racer_name_sample.rb
@@ -1,8 +1,18 @@
+require 'nkf'
+
 class RacerNameSample < ApplicationRecord
 
   validates :name, presence: true, length: {in: 2 .. 9}
 
+  after_initialize :name_to_katakana
+
   def to_s
     name
   end
+
+  private
+
+    def name_to_katakana
+      self.name = NKF.nkf('-w --katakana', name) if name
+    end
 end
