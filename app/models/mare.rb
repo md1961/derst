9fbb5203527f1cae933a264b55a8c9c56514a5ac
Naoyuki Kumagai <kumagai@namaka>
2019-10-23 15:18:55 +0900

    Add has_many :racers to Mare

--- app/models/mare.rb
+++ app/models/mare.rb
@@ -6,6 +6,7 @@ class Mare < ApplicationRecord
   belongs_to :father, class_name: 'Sire', optional: true
   belongs_to :lineage
   has_many :maternal_lines, class_name: 'MareMaternalLine', dependent: :destroy
+  has_many :racers, foreign_key: 'mother_id'
 
   def to_s
     name
