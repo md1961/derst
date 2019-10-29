60b434cf3810027f717b32b876346ed9284717f1
Naoyuki Kumagai <kumagai@namaka>
2019-10-29 15:17:42 +0900

    Add RacerNameSample.combinations() preliminarily

--- app/models/racer_name_sample.rb
+++ app/models/racer_name_sample.rb
@@ -1,8 +1,21 @@
+require 'set'
+
 class RacerNameSample < ApplicationRecord
 
   validates :name, presence: true, length: {in: 2 .. 9}
 
   after_initialize :name_to_katakana
+
+  def self.combinations(num = 1)
+    set = Set.new
+    while set.size < num
+      name0 = pluck(:name).find_all { |name| name.length <= 7                }.sample
+      name1 = pluck(:name).find_all { |name| name.length <= 9 - name0.length }.sample
+      names = [name0, name1].sort
+      set << [names.join, names.reverse.join]
+    end
+    set
+  end
 
   def to_s
     name
