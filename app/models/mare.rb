ceb4e3bed7cad5134f3afa6cc7c2d9aa137ce460
Naoyuki Kumagai <kumagai@namaka>
2019-11-14 10:28:23 +0900

    Extract Mare.create_from_mating_of() from Racer#create_mare()

--- app/models/mare.rb
+++ app/models/mare.rb
@@ -8,6 +8,18 @@ class Mare < ApplicationRecord
   has_many :maternal_lines, class_name: 'MareMaternalLine', dependent: :destroy
   has_many :racers, foreign_key: 'mother_id'
 
+
+  def self.create_from_mating_of(mother, father, name = nil)
+    name = "#{mother.name} x #{father.name}" unless name
+    Mare.transaction do
+      Mare.create!(name: name, father: father, lineage: father.trait.lineage).tap { |mare|
+        mare.maternal_lines.create!(generation: 2, father: mother.father)
+        mare.maternal_lines.create!(generation: 3, father: mother.bloodline_father(2, 2))
+        mare.maternal_lines.create!(generation: 4, father: mother.bloodline_father(3, 4))
+      }
+    end
+  end
+
   def to_s
     name
   end
