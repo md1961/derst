d6c33b9458245f004514cb650f0e36649e4bbf74
Naoyuki Kumagai <kumagai@namaka>
2019-11-21 11:35:03 +0900

    Modify to reject access from client other than the developer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate
 
   private
+
+    HOSTNAMES_TO_REJECT = %w[namaka mimas sinope rael ariel]
+    IP_ADDRESSES_TO_ACCEPT = %w[192.168.34.162]
+
+    def authenticate
+      if HOSTNAMES_TO_REJECT.include?(`hostname`.chomp) \
+          && !IP_ADDRESSES_TO_ACCEPT.include?(ip_address_of_client)
+        render plain: 'Rejected'
+        return
+      end
+    end
+
+    def ip_address_of_client
+      request.env["HTTP_X_FORWARDED_FOR"] || request.remote_ip
+    end
 
     def sire_filter_params
       return {} unless params[:sire_filter]
