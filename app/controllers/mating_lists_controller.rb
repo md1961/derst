ba731dd4f7849c541fee2e4edc3f9bd35051c9ed
Naoyuki Kumagai <kumagai@namaka>
2020-01-09 09:16:39 +0900

    Modify 'mating_lists#' to handle sex of racer on sale

--- app/controllers/mating_lists_controller.rb
+++ app/controllers/mating_lists_controller.rb
@@ -1,5 +1,5 @@
 class MatingListsController < ApplicationController
-  before_action :set_mating, only: %i[update delete prev next]
+  before_action :set_mating_list_item, only: %i[update delete prev next]
   before_action :set_mating_list
 
   KEY_MATING_LIST_IN_JSON = :mating_list_in_json
@@ -13,8 +13,8 @@ class MatingListsController < ApplicationController
 
   def update
     _params = {}
-    if @mating
-      @mating_list << @mating
+    if @mating_list_item.valid?
+      @mating_list << @mating_list_item
       save_mating_list
     else
       _params = params.permit(:mare, :sire)
@@ -23,7 +23,7 @@ class MatingListsController < ApplicationController
   end
 
   def delete
-    @mating_list.delete(@mating)
+    @mating_list.delete(@mating_list_item)
     save_mating_list
     redirect_to mating_list_path
   end
@@ -35,21 +35,24 @@ class MatingListsController < ApplicationController
   end
 
   def prev
-    mating = @mating_list.prev_of(@mating)
+    list_item = @mating_list.prev_of(@mating_list_item.mating)
+    mating = list_item.mating
     redirect_to mating_path(mating.mare.id, sire_id: mating.sire.id, in_list: true)
   end
 
   def next
-    mating = @mating_list.next_of(@mating)
+    list_item = @mating_list.next_of(@mating_list_item.mating)
+    mating = list_item.mating
     redirect_to mating_path(mating.mare.id, sire_id: mating.sire.id, in_list: true)
   end
 
   private
 
-    def set_mating
+    def set_mating_list_item
       mare = Mare.find_by(name: params[:mare]) || Mare.find_by(id: params[:mare_id])
       sire = Sire.find_by_name( params[:sire]) || Sire.find_by(id: params[:sire_id])
-      @mating = mare && sire ? Mating.new(mare, sire) : nil
+      mating = mare && sire ? Mating.new(mare, sire) : nil
+      @mating_list_item = MatingList::Item.new(mating, params[:sex].to_i)
     end
 
     def set_mating_list
--- app/models/mating_list.rb
+++ app/models/mating_list.rb
@@ -1,59 +1,67 @@
 class MatingList
 
+  Item = Struct.new(:mating, :sex) do
+    def valid?
+      mating.is_a?(Mating) && [1, 2].include?(sex)
+    end
+  end
+
   def self.build_from_json(json)
     json = "[]" if json.blank?
-    new(JSON.parse(json).map { |mare_id, sire_id|
-      Mating.new(Mare.find(mare_id), Sire.find(sire_id))
+    new(JSON.parse(json).map { |mare_id, sire_id, sex|
+      mating = Mating.new(Mare.find(mare_id), Sire.find(sire_id))
+      list_item = Item.new(mating, sex)
     })
   end
 
-  def initialize(matings)
-    @matings = matings
+  def initialize(list_items)
+    @list_items = list_items
   end
 
   def empty?
-    @matings.empty?
+    @list_items.empty?
   end
 
   def size
-    @matings.size
+    @list_items.size
   end
 
   def each(&block)
-    @matings.each(&block)
+    @list_items.each(&block)
   end
 
   def prev_of(mating)
-    return nil if @matings.empty?
-    index = @matings.find_index(mating)
+    return nil if @list_items.empty?
+    index = @list_items.find_index { |item| item.mating == mating }
     return nil unless index
-    @matings[index - 1]
+    @list_items[index - 1]
   end
 
   def next_of(mating)
-    return nil if @matings.empty?
-    index = @matings.find_index(mating)
+    return nil if @list_items.empty?
+    index = @list_items.find_index { |item| item.mating == mating }
     return nil unless index
-    @matings[(index + 1) % @matings.size]
+    @list_items[(index + 1) % @list_items.size]
   end
 
-  def <<(mating)
-    @matings << mating
+  def <<(list_item)
+    @list_items << list_item
     self
   end
 
-  def delete(mating)
-    @matings.delete(mating)
+  def delete(list_item)
+    @list_items.delete(list_item)
     self
   end
 
   def clear
-    @matings = []
+    @list_items = []
   end
 
   def to_json
-    JSON.generate(@matings.map { |mating|
-      [mating.mare.id, mating.sire.id]
+    JSON.generate(@list_items.map { |list_item|
+      mating = list_item.mating
+      [mating.mare.id, mating.sire.id, list_item.sex]
     })
   end
 end
