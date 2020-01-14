c5744e46cf9de4a9900cadb96ea6d00e2d6209ef
Naoyuki Kumagai <kumagai@namaka>
2020-01-14 14:11:07 +0900

    Modify PostRace to injure racer after save according to comment

--- app/models/post_race.rb
+++ app/models/post_race.rb
@@ -1,3 +1,13 @@
 class PostRace < ApplicationRecord
   belongs_to :result
+
+  after_save :injure_racer
+
+  private
+
+    def injure_racer
+      description = comment.to_s.split.last
+      return if description.blank?
+      result.racer.injure(description)
+    end
 end
