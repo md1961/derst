3b65be50140609fcab53b777a30dd36c260a4395
Naoyuki Kumagai <kumagai@namaka>
2020-01-15 11:33:22 +0900

    Add Stats.each_most_number_of_equal_or_better_places_of()

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -8,7 +8,7 @@ module Stats
   }
 
   def each_result_in_row_of_equal_or_better_place_of(place, high_stakes: false, &block)
-    Racer.all.flat_map { |racer|
+    Racer.all.includes(results: {race: :grade}).flat_map { |racer|
       racer.results.in_row_of_equal_or_better_place_of(place, high_stakes: high_stakes)
     }.find_all { |results|
       results.size >= MIN_IN_ROW_OF_PLACE[place] - (high_stakes ? 1 : 0)
@@ -18,14 +18,30 @@ module Stats
   end
 
   def each_oldest_high_stake_win(&block)
-    Racer.all.map { |racer|
+    Racer.all.includes(results: {race: :grade}).map { |racer|
       racer.results.high_stake.wins&.last
     }.compact.sort_by(&:age_in_week).reverse.each(&block)
   end
 
   def each_youngest_high_stake_win(&block)
-    Racer.all.map { |racer|
+    Racer.all.includes(results: {race: :grade}).map { |racer|
       racer.results.high_stake.wins&.first
     }.compact.sort_by(&:age_in_week).each(&block)
+  end
+
+  MIN_NUMBER_OF_PLACES = {
+    1 => [ 6,  2],
+    2 => [10,  2],
+    3 => [13,  3]
+  }
+
+  def each_most_number_of_equal_or_better_places_of(place, high_stakes: false, &block)
+    Racer.all.includes(results: {race: :grade}).map { |racer|
+      [racer, racer.place_records(high_stakes: high_stakes)[1 .. place].sum]
+    }.find_all { |_, number|
+      number >= MIN_NUMBER_OF_PLACES[place][high_stakes ? 1 : 0]
+    }.sort_by { |_, number|
+      -number
+    }.each(&block)
   end
 end
