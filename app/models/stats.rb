61e047979d081074e0a9b6721137b01e927fce06
Naoyuki Kumagai <kumagai@namaka>
2020-01-15 10:52:13 +0900

    Modify Stats.each_result_in_row_of_equal_or_better_place_of() to take argument high_stakes

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -7,11 +7,11 @@ module Stats
     3 => 5
   }
 
-  def each_result_in_row_of_equal_or_better_place_of(place, &block)
+  def each_result_in_row_of_equal_or_better_place_of(place, high_stakes: false, &block)
     Racer.all.flat_map { |racer|
-      racer.results.in_row_of_equal_or_better_place_of(place)
+      racer.results.in_row_of_equal_or_better_place_of(place, high_stakes: high_stakes)
     }.find_all { |results|
-      results.size >= MIN_IN_ROW_OF_PLACE[place]
+      results.size >= MIN_IN_ROW_OF_PLACE[place] - (high_stakes ? 1 : 0)
     }.sort_by { |results|
       -results.size
     }.each(&block)
