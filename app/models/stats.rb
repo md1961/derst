7118fed28eeb3b10312a681622797ad09b21c19c
Naoyuki Kumagai <kumagai@namaka>
2020-01-15 13:45:07 +0900

    Modify Stats to take cetain number of top ranker including ties

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -2,16 +2,16 @@ module Stats
   module_function
 
   MIN_IN_ROW_OF_PLACE = {
-    1 => 3,
-    2 => 4,
-    3 => 5
+    1 => [3, 2],
+    2 => [4, 3],
+    3 => [5, 4]
   }
 
   def each_result_in_row_of_equal_or_better_place_of(place, high_stakes: false, &block)
     Racer.all.includes(results: {race: :grade}).flat_map { |racer|
       racer.results.in_row_of_equal_or_better_place_of(place, high_stakes: high_stakes)
     }.find_all { |results|
-      results.size >= MIN_IN_ROW_OF_PLACE[place] - (high_stakes ? 1 : 0)
+      results.size >= MIN_IN_ROW_OF_PLACE[place][high_stakes ? 1 : 0]
     }.sort_by { |results|
       -results.size
     }.each(&block)
@@ -29,31 +29,21 @@ module Stats
     }.compact.sort_by(&:age_in_week).each(&block)
   end
 
-  MIN_NUMBER_OF_PLACES = {
-    1 => [ 6,  2],
-    2 => [10,  2],
-    3 => [13,  3]
-  }
-
   def each_most_number_of_equal_or_better_places_of(place, high_stakes: false, &block)
+    counter = Counter.new
     Racer.all.includes(results: {race: :grade}).map { |racer|
       [racer, racer.place_records(high_stakes: high_stakes)[1 .. place].sum]
-    }.find_all { |_, number|
-      number >= MIN_NUMBER_OF_PLACES[place][high_stakes ? 1 : 0]
     }.sort_by { |_, number|
       -number
+    }.take_while { |_, number|
+      counter.count(number)
     }.each(&block)
   end
 
   MIN_RACES_FOR_BEST_PCT = [10, 5]
 
-  MIN_PCT_OF_PLACES = {
-    1 => [0.25, 0.00],
-    2 => [0.35, 0.00],
-    3 => [0.45, 0.00]
-  }
-
   def each_best_pct_of_equal_or_better_places_of(place, high_stakes: false, &block)
+    counter = Counter.new
     Racer.all.includes(results: {race: :grade}).map { |racer|
       place_records = racer.place_records(high_stakes: high_stakes)
       num_races = place_records[0]
@@ -62,10 +52,25 @@ module Stats
       else
         [racer, place_records[1 .. place].sum, num_races]
       end
-    }.compact.find_all { |_, num_places, num_races|
-      num_places.to_f / num_races >= MIN_PCT_OF_PLACES[place][high_stakes ? 1 : 0]
-    }.sort_by { |_, num_places, num_races|
+    }.compact.sort_by { |_, num_places, num_races|
       -num_places.to_f / num_races
+    }.take_while { |_, num_places, num_races|
+      counter.count(num_places.to_f / num_races)
     }.each(&block)
   end
+
+    class Counter
+
+      def initialize(max_count = 10)
+        @max_count = max_count
+        @count = 0
+        @value_to_cut = 0
+      end
+
+      def count(value)
+        @count += 1
+        @value_to_cut = value if @count == @max_count
+        @count <= @max_count || value >= @value_to_cut
+      end
+    end
 end
