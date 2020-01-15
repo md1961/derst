61ee71cb35af79c8e9d1cb714f36a642ebdc4321
Naoyuki Kumagai <kumagai@namaka>
2020-01-15 09:08:06 +0900

    Modify helper major_wins_display() to emphasize G1 results

--- app/helpers/ranches_helper.rb
+++ app/helpers/ranches_helper.rb
@@ -8,9 +8,15 @@ module RanchesHelper
   def major_wins_display(racer)
     racer.major_wins.map { |results|
       results.map { |result|
-        race = result.race
-        place_display = result.place == 1 ? "" : " #{result.place}着"
-        "#{race.name}(#{race.distance_to_s}, #{result.age}歳)#{place_display}"
+        race  = result.race
+        place = result.place
+        place_display = place == 1 ? "" : " #{place}着"
+        classes = []
+        classes << 'g1'  if race.grade.g1?
+        classes << 'win' if place == 1
+        content_tag(:span, class: classes.join(' ')) {
+          "#{race.name}(#{race.distance_to_s}, #{result.age}歳)#{place_display}"
+        }
       }.join(', ').yield_self { |x| x.blank? ? nil : x }
     }.compact.join('<br>').html_safe
   end
