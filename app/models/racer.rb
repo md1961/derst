f80c385bd94e07984edccdbca05db7c91521f2c4
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2020-01-25 16:03:01 +0900

    Modify Racer to convert remark of number to monetary display on save

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -49,6 +49,8 @@ class Racer < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
 
+  before_save :convert_remark_to_total_prize
+
   def self.num_in_ranch
     active.count - in_stable.count - num_in_spa
   end
@@ -350,4 +352,10 @@ class Racer < ApplicationRecord
       } || 0
     end
   end
+
+  private
+
+    def convert_remark_to_total_prize
+      self.remark = StringUtil.monetary_display(remark) if remark.to_i > 1000
+    end
 end
