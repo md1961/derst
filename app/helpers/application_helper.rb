6b61dfd33f70b8fd3be77556d0202a0d4d4ee498
Naoyuki Kumagai <kumagai@namaka>
2020-01-31 13:23:06 +0900

    Add helper form_for_weekly_weight()

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -337,7 +337,20 @@ module ApplicationHelper
                                     autofocus: racer.id == flash[:racer_id_to_focus],
                                     tabindex: racer.condition ? -1 : 0
       concat submit_tag :enter, hidden: 'hidden'
-      concat hidden_field_tag :ranch_id, @ranch&.id, id: "ranch_id-#{racer.id}"
+      concat hidden_field_tag :ranch_id, @ranch&.id, id: "ranch_id-#{racer.id}-condition"
+    end
+  end
+
+  def form_for_weekly_weight(racer)
+    form_with url: weekly_racer_path(racer), local: true, class: 'weekly_weight' do
+      concat number_field_tag :weight, racer.weight || racer.last_weight,
+                                          step: 2,
+                                          disabled: @racer_id_to_edit.to_i > 0,
+                                          id: "weight-#{racer.id}",
+                                          class: ['weight', racer.weight.nil? ? 'no_weight' : ''],
+                                          tabindex: racer.condition && !racer.weight ? 0 : -1
+      concat submit_tag :enter, hidden: 'hidden'
+      concat hidden_field_tag :ranch_id, @ranch&.id, id: "ranch_id-#{racer.id}-weight"
     end
   end
 
