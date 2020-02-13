b5a21a350be78ef0f437f2a1450cc194f6c5a0a6
Naoyuki Kumagai <kumagai@namaka>
2020-02-14 08:39:18 +0900

    Remove unnecessary codes for age-2-or-1 racers toggling

--- app/controllers/ranches_controller.rb
+++ app/controllers/ranches_controller.rb
@@ -1,17 +1,10 @@
 class RanchesController < ApplicationController
 
   KEY_SHOWS_MARES = :shows_mares_in_ranches_show
-  KEY_SHOWS_NO_STABLE = :shows_no_stable_in_ranches_show
 
   def show
     @ranch = Ranch.last
     @mares = @ranch.mares.order(ranch_mare_sort_key)
-
-    @shows_no_stable = params[:shows_no_stable] ? params[:shows_no_stable] == 'true' \
-                                                : session[KEY_SHOWS_NO_STABLE]
-    session[KEY_SHOWS_NO_STABLE] = @shows_no_stable
-    # FIXME:
-    @shows_no_stable = Racer.find_by(id: flash[:racer_id_to_focus]).yield_self { |racer| !(racer && racer.age <= 2) }
 
     @main_display = params[:main_display].yield_self { |x| x.blank? ? nil : x }
 
@@ -25,7 +18,10 @@ class RanchesController < ApplicationController
     end
 
     @racer_id_to_edit = params[:racer_id_to_edit].to_i
-    @shows_no_stable = false if @racer_id_to_edit > 0
+
+    @shows_age2_or_1 = (Racer.find_by(id: params[:racer_id_to_edit]) \
+                     || Racer.find_by(id: flash[:racer_id_to_focus]))
+                          .yield_self { |racer| racer && racer.age <= 2 }
 
     @shows_mares = session[KEY_SHOWS_MARES]
 
