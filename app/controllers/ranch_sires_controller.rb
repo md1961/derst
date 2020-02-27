ae0c88d79fb0d037bb50da937eba0cbee4d18e75
Naoyuki Kumagai <kumagai@namaka>
2020-02-28 08:53:56 +0900

    Modify top bar to display link to 'ranch_sires#index'

--- app/controllers/ranch_sires_controller.rb
+++ app/controllers/ranch_sires_controller.rb
@@ -1,20 +1,25 @@
 class RanchSiresController < ApplicationController
+  before_action :set_ranch
 
   def index
-    @ranch = Ranch.last
     @ranch_sires = @ranch.ranch_sires
   end
 
   def create
-    ranch = Ranch.find(params[:ranch_id])
     sire = Sire.find_by_name(params[:sire])
-    ranch.ranch_sires.create!(sire: sire) if sire
-    redirect_to ranch_sires_path
+    @ranch.ranch_sires.create!(sire: sire) if sire
+    redirect_to ranch_sires_path(ranch_id: @ranch)
   end
 
   def destroy
     ranch_sire = RanchSire.find(params[:id])
     ranch_sire.destroy
-    redirect_to ranch_sires_path
+    redirect_to ranch_sires_path(ranch_id: @ranch)
   end
+
+  private
+
+    def set_ranch
+      @ranch = Ranch.find(params[:ranch_id])
+    end
 end
