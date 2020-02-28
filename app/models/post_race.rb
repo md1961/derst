b717b21d308c03c392a40ece46cce93ff6120bf9
Naoyuki Kumagai <kumagai@namaka>
2020-02-28 11:04:44 +0900

    Add PostRace#description() and #injury?()

--- app/models/post_race.rb
+++ app/models/post_race.rb
@@ -3,17 +3,24 @@ class PostRace < ApplicationRecord
 
   after_save :injure_racer
 
+  RE_DESCRIPTION_FOR_INJURY = /(?:重傷|骨折|屈腱炎|ハ行|ソエ)\z/
+
+  def description
+    @description ||= comment.to_s.split.last
+  end
+
+  def injury?
+    description =~ RE_DESCRIPTION_FOR_INJURY
+  end
+
   private
 
-    RE_DESCRIPTION_FOR_INJURY = /(?:骨折|屈腱炎|ハ行|ソエ)\z/
-
     def injure_racer
-      description = comment.to_s.split.last
       return if description.blank?
       if description == '去勢'
         result.racer.gelding!
         return
       end
-      result.racer.injure(description) if description =~ RE_DESCRIPTION_FOR_INJURY
+      result.racer.injure(description) if injury?
     end
 end
