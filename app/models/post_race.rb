f44c7a6d96c58798a8468af9dffaf8377823b47f
Naoyuki Kumagai <kumagai@namaka>
2020-02-28 11:59:23 +0900

    Add PostRace#age_in_week()

--- app/models/post_race.rb
+++ app/models/post_race.rb
@@ -13,6 +13,19 @@ class PostRace < ApplicationRecord
     description =~ RE_DESCRIPTION_FOR_INJURY
   end
 
+  def age_in_week
+    age_in_week = result.age_in_week
+    if m = comment.match(/\A(\d+)\D(\d+)/)
+      month = m[1].to_i
+      week  = m[2].to_i
+      age = age_in_week.age
+      age += 1 if month < age_in_week.month || (month == age_in_week.month && week < age_in_week.week)
+      Racer::AgeInWeek.new(age, month, week)
+    else
+      age_in_week
+    end
+  end
+
   private
 
     def injure_racer
