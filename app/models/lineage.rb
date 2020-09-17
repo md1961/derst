3a37c8007893376a4ff27ff955e8a937319ed4e1
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2020-09-17 09:45:19 +0900

    Add Lineage#nicks_lineages()

--- app/models/lineage.rb
+++ app/models/lineage.rb
@@ -1,5 +1,10 @@
 class Lineage < ApplicationRecord
 
+  def nicks_lineages
+    Nick.where(lineage1: self).map(&:lineage2) + \
+    Nick.where(lineage2: self).map(&:lineage1)
+  end
+
   def to_s
     name
   end
