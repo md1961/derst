33ceb672e7137848ac2f3adbf3edc6701bbecf7b
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2020-11-27 19:09:36 +0900

    Comment out whole body of concern SireFatherCache not in use

--- app/models/concerns/sire_father_cache.rb
+++ app/models/concerns/sire_father_cache.rb
@@ -1,11 +1,11 @@
 module SireFatherCache
 
-  refine Sire do
-
-    H_FATHERS = Sire.all.map { |s| [s.id, Sire.find_by(id: s.father_id)] }.to_h
-
-    def father
-      H_FATHERS[id]
-    end
-  end
+#  refine Sire do
+#
+#    H_FATHERS = Sire.all.map { |s| [s.id, Sire.find_by(id: s.father_id)] }.to_h
+#
+#    def father
+#      H_FATHERS[id]
+#    end
+#  end
 end
