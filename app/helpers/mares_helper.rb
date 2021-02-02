4d43260d0b2fb474d76c1c2c1f9b4c5c1678c29d
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2021-02-02 09:17:22 +0900

    Modify 'mares#potentials' to display mothers, root lineage numbers, etc.

--- app/helpers/mares_helper.rb
+++ app/helpers/mares_helper.rb
@@ -1,2 +1,8 @@
 module MaresHelper
+
+  def g1_wins_display(kind, racer)
+    return (kind == :count ? '－' : '') unless racer
+    results = racer.results.wins.find_all { |result| result.race.grade.g1? }
+    kind == :count ? "#{results.size}勝" : results.map(&:race).map(&:name).join('、')
+  end
 end
