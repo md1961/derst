3e42606f7f456448e334c66d594b19bd253049a2
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2022-01-26 15:08:33 +0900

    Add RanchSire.destroy_lease_expiring()

--- app/models/ranch_sire.rb
+++ app/models/ranch_sire.rb
@@ -2,6 +2,13 @@ class RanchSire < ApplicationRecord
   belongs_to :ranch
   belongs_to :sire
 
+  def self.destroy_lease_expiring
+    return if count.zero?
+    ranch = first.ranch
+    return if ranch.month < 6
+    all.find_all { |ranch_sire| ranch_sire.year_left == 1 }.map(&:destroy)
+  end
+
   def year_left
     year_leased + 5 - ranch.year
   end
