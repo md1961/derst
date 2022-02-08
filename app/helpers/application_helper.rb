44a65bfb59a08aef9a658dfc0e56bed70287b985
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2022-02-09 08:39:53 +0900

    Apply application template for helper error_messages_for()

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -490,4 +490,9 @@ module ApplicationHelper
       lineages.empty? ? display_when_empty : lineages.map(&:name).join(', ')
     }
   end
+
+  def error_messages_for(model_instance)
+    return if model_instance.errors.empty?
+    render 'error_messages_for', model_instance: model_instance
+  end
 end
